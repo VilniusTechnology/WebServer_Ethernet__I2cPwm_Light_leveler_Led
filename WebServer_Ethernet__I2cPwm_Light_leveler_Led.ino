01191f498a1c16ed37aaa9bc9bf0eabd8527faf9
Lukas Mikelionis <lukas.mikelionis@nfq.com>
2018-08-17 20:52:59 +0300

    Init

--- WebServer_Ethernet__I2cPwm_Light_leveler_Led.ino
+++ WebServer_Ethernet__I2cPwm_Light_leveler_Led.ino
@@ -1,8 +1,13 @@
-//#include <RGB_Driver.h>
 #include <I2C.h>
 #include <EtherCard.h>
+
+#include <Wire.h>
+#include "PCA9685.h"
+
 #include <Get_params_parser.h>
 
+// Library using default Wire and default linear phase balancing scheme
+PCA9685 pwmController;                  
 
 // LIGHTING
 String ledStatus = "1";
@@ -11,7 +16,7 @@ String mode = "1";
 String coldWhite = "0";
 String warmWhite = "0";
 
-String red = "150";
+String red = "110";
 String green = "0";
 String blue = "0";
 
@@ -24,7 +29,7 @@ int coldWhiteLevelMax = 250;
 int coldWhiteLevelLockMin = 5;
 int span = 1500;
 
-int testTimeOffset = 100;
+int testTimeOffset = 30;
 
 String rgbDriverMode = "";
 
@@ -40,122 +45,59 @@ BufferFiller bfill;
 int timeOffset = 2500;
 int sensVal = 0;
 
+// PINS are on PCA9685 I2C 16-channel PWM driver module
+uint8_t whitePin = 0;
+uint8_t warmWhitePin = 1;
+
+uint8_t redPin = 2;
+uint8_t greenPin = 3;
+uint8_t bluePin = 4;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println(F("====== SETUP HAD BEGAN ========"));
+  logLn(F("====== SETUP HAD BEGAN ========"));
 
   // Scan only to get address ONCE !!!
   //  I2c.scan();
-
   
   // ETHERCARD SETUP
   if (ether.begin(sizeof Ethernet::buffer, mymac, 53) == 0) {
-    Serial.println(F("Failed to access Ethernet controller"));
+    logLn(F("Failed to access Ethernet controller"));
   }
   ether.staticSetup(myip);
   ether.printIp("IP:  ", ether.myip);
 
-  // Setup sensor for light level monitoring
-  I2c.begin();
+//  // Setup sensor for light level monitoring
+//  I2c.begin();
+//  
+//  I2c.write(0x4A, 0x01, 0x0);
+//  I2c.write(0x4A, 0x02, 0x00);
+
+  logLn(F("====== BASIC SETUP COMPLETED ========"));
+  delay(500);
+  logLn(F("====== WILL SET PWM GENERATOR ========"));
+
+  Wire.begin();                       // Wire must be started first
+  Wire.setClock(400000);              // Supported baud rates are 100kHz, 400kHz, and 1000kHz
   
-  I2c.write(0x4A, 0x01, 0x0);
-  I2c.write(0x4A, 0x02, 0x00);
-
-  // resetLedOutputs();
-
-  Serial.println(F("====== SETUP COMPLETED ========"));
-  delay(3000);
+  pwmController.resetDevices();       // Software resets all PCA9685 devices on Wire line
+  pwmController.init(B000000);        // Address pins A5-A0 set to B000000
+  pwmController.setPWMFrequency(200); // Default is 200Hz, supports 24Hz to 1526Hz
+  
+  logLn(F("====== SET PWM GENERATOR DONE ========"));
+  logLn(F("====== FULL SETUP COMPLETED ========"));
 }
 
 void loop() {
-
-  //testRgbCCTCycle();
-  //delay(5000);
-  
   word len = ether.packetReceive();
   
-  // resetLedOutputs(); // !!! IMPORTANT TO AVOID WHITE FLAS, AS MISO PIN IS USED FOR WHITE
   execute_RGB_program(ledStatus.toInt(), mode.toInt(), coldWhite.toInt(), warmWhite.toInt(), red.toInt(), green.toInt(), blue.toInt());
   
   word pos = ether.packetLoop(len);
     
   // check if valid tcp data is received
   if (pos) {
-    bfill = ether.tcpOffset();
-
-    char* data = (char *) Ethernet::buffer + pos;
-
-    // Get your params.
-    Get_params_parser *parmParser  = new Get_params_parser(data);
-
-    String ledStatusTmp = parmParser->getParamValue("&1");
-    
-   if(isDigit(ledStatusTmp.charAt(0))) {
-
-//    Serial.println(F("The ledStatusTmp was a number"));
-
-    String ledStatusOld = ledStatus;
-
-//    Serial.print(F("Old LED STATUS IS: "));
-//    Serial.println(ledStatusOld);
-
-    if (ledStatusTmp.toInt() == 3) {
-      Serial.println(F("The ledStatusTmp was a number 3"));
-      ledStatus = ledStatusOld;
-    } else {
-      ledStatus = ledStatusTmp;
-    }
-
-//    Serial.print(F("New LED STATUS IS: "));
-//    Serial.println(ledStatus);
-    
-    if (ledStatus.toInt() == 1) {
-
-      String modeTmp = parmParser->getParamValue("&2");     
-      String coldWhiteTmp = parmParser->getParamValue("&3");
-      String warmWhiteTmp = parmParser->getParamValue("&4");
-      String redTmp = parmParser->getParamValue("&5");
-      String greenTmp = parmParser->getParamValue("&6");
-      String blueTmp = parmParser->getParamValue("&7");
-  
-      delete parmParser;
-    
-    
-        if (isDigit(modeTmp.charAt(0))) {
-//          Serial.println(F("The modeTmp was a number"));
-//          Serial.println(modeTmp);
-            mode = modeTmp;
-        }
-    
-        if (isDigit(coldWhiteTmp.charAt(0))) {
-          coldWhite = coldWhiteTmp;
-        }
-    
-        if (isDigit(warmWhiteTmp.charAt(0))) {
-          warmWhite = warmWhiteTmp;
-        }
-    
-        if (isDigit(redTmp.charAt(0))) {
-          red = redTmp;
-        }
-    
-        if (isDigit(greenTmp.charAt(0))) {
-          green = greenTmp;
-        }
-    
-        if (isDigit(blueTmp.charAt(0))) {
-          blue = blueTmp;
-        }
-    } 
-    
-    if (ledStatus.toInt() == 0) {
-      ledStatus = "0";
-    }
-
-  }
-    word rspns = httpResponse(sensVal, ledStatus.toInt(), mode.toInt(), coldWhite.toInt(), warmWhite.toInt(), red.toInt(), green.toInt(), blue.toInt());
-    ether.httpServerReply(rspns);
+    handleNewValues(pos);
   }
   
   I2c.read(0x4A, 0x3, 6);
@@ -166,42 +108,91 @@ void loop() {
 }
 
 void execute_RGB_program(int state, int mode, int ww, int cw, int red, int green, int blue) {
-  Serial.print(F("====== execute_RGB_program STATE = "));
-  Serial.println(state);
-  delay(100);
-  Serial.print(F("====== execute_RGB_program MODE = "));
-  Serial.println(mode);
+  logLn(F("====== execute_RGB_program STATE = "));
+  logLn(String(state));
+  logLn(F("====== execute_RGB_program MODE = "));
+  logLn(String(mode));
   
   if(state == 1) {
     if (mode == 0) {
       // ADAPTIVE
-      Serial.println(F("====== WILL SET AJUST ========"));
+      logLn(F("====== WILL SET AJUST ========"));
 //      adjustLight();
     } else if(mode == 1) {
       // MANUAL
-      Serial.println(F("====== WILL SET MANUAL MODE 1 ========"));
+      logLn(F("====== WILL SET MANUAL MODE 1 ========"));
       setManualMode(ww, cw, red, green, blue);
     } else if(mode == 2) {
       // TIMED
     } else if(mode == 3) {
-      Serial.println(F("====== WILL SET DEMO ========"));
+      logLn(F("====== WILL SET DEMO ========"));
       testRgbCCTCycle();
     }
   } else {
-    Serial.println(F("====== LEDS ARE OFF ========"));
+    logLn(F("====== WILL TURN LEDS OFF ========"));
     turnLedsOff();
   }
+}
 
-    Serial.println(F("Current settings"));
-//    Serial.println(ledStatus);
-//    Serial.println(mode);
-    Serial.println(coldWhite);
-    Serial.println(warmWhite);
-    Serial.println(red);
-    Serial.println(green);
-    Serial.println(blue);
-    Serial.println(F("..."));
-//    delay(500);/
+void handleNewValues(word pos) {
+  logLn(F("HAR RECEIVED SOME DATA !!! "));
+    
+  bfill = ether.tcpOffset();
+
+  char* data = (char *) Ethernet::buffer + pos;
+
+  // Get your params.
+  Get_params_parser *parmParser  = new Get_params_parser(data);
+  
+  String ledStatusTmp = parmParser->getParamValue("&1");
+    
+  if(isDigit(ledStatusTmp.charAt(0))) {
+    String ledStatusOld = ledStatus;
+  
+    if (ledStatusTmp.toInt() == 3) {
+      logLn(F("The ledStatusTmp was a number 3"));
+      ledStatus = ledStatusOld;
+    } else {
+      ledStatus = ledStatusTmp;
+    }
+  
+    if (ledStatus.toInt() == 1) {
+      String modeTmp = parmParser->getParamValue("&2");     
+      String coldWhiteTmp = parmParser->getParamValue("&3");
+      String warmWhiteTmp = parmParser->getParamValue("&4");
+      String redTmp = parmParser->getParamValue("&5");
+      String greenTmp = parmParser->getParamValue("&6");
+      String blueTmp = parmParser->getParamValue("&7");
+
+      delete parmParser;
+  
+      if (isDigit(modeTmp.charAt(0))) {
+          mode = modeTmp;
+      }
+      if (isDigit(coldWhiteTmp.charAt(0))) {
+        coldWhite = coldWhiteTmp;
+      }
+      if (isDigit(warmWhiteTmp.charAt(0))) {
+        warmWhite = warmWhiteTmp;
+      }
+      if (isDigit(redTmp.charAt(0))) {
+        red = redTmp;
+      }
+      if (isDigit(greenTmp.charAt(0))) {
+        green = greenTmp;
+      }
+      if (isDigit(blueTmp.charAt(0))) {
+        blue = blueTmp;
+      }
+    } 
+  
+    if (ledStatus.toInt() == 0) {
+      ledStatus = "0";
+    }
+  }
+    
+  word rspns = httpResponse(sensVal, ledStatus.toInt(), mode.toInt(), coldWhite.toInt(), warmWhite.toInt(), red.toInt(), green.toInt(), blue.toInt());
+  ether.httpServerReply(rspns);
 }
 
 void turnLedsOff() {
@@ -210,45 +201,38 @@ void turnLedsOff() {
   setRed(0);
   setGreen(0);
   setBlue(0); 
+  logLn(F("====== LEDS ARE OFF ========"));
 }
 
 void setManualMode(int ww, int cw, int red, int green, int blue) {
-  Serial.println(F("====== WILL SET MANUAL MODE 2 ========"));
-//  delay(500);
   setColdWhite(ww);
   setWarmWhite(cw);
   setRed(red);
   setGreen(green);
   setBlue(blue);
-  delay(10);
 }
 
 void setColor(uint8_t pin, int value ) {
-  analogWrite(pin, value);
+  pwmController.setChannelPWM(pin, value); // Set PWM channel 0 to 255
 }
 
 void setColdWhite(int value) {
-  uint8_t whitePinn = 2;
-  setColor(whitePinn, value);
+  setColor(whitePin, value);
 }
 
 void setWarmWhite(int value) {
-  uint8_t warmWhite = 6;
-  setColor(warmWhite, value);
+  setColor(warmWhitePin, value);
 }
 
 void setRed(int value) {
-  uint8_t redPin = 4;
   setColor(redPin, value);
 }
 
 void setGreen(int value) {
-  uint8_t greenPin = 7;
   setColor(greenPin, value);
 }
 
 void setBlue(int value) {
-  uint8_t bluePin = 9;
   setColor(bluePin, value);
 }
 
@@ -285,7 +269,7 @@ static word httpResponse(int sensorValue, int state, int mode, int coldWhite, in
     "Access-Control-Allow-Origin: *\r\n"
     "\r\n"
       "{"
-      "\"version\": \"0.0.1\","
+      "\"version\": \"0.0.2\","
       "\"reading\": \"$D\","
       "\"state\": \"$D\","
       "\"mode\": \"$D\","
@@ -297,27 +281,21 @@ static word httpResponse(int sensorValue, int state, int mode, int coldWhite, in
       "}"
     ), 
     sensorValue, state, mode, coldWhite, warmWhite, red, green, blue);
-    Serial.println("---- RESPONSE WAS SENT  ---- ");
+    logLn("---- RESPONSE WAS SENT  ---- ");
     return bfill.position();
 }
 
-void resetLedOutputs() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
+void logInLn(String text) {
+  // Serial.print(text);
+}
 
-  analogWrite(2, 0);
-  analogWrite(3, 0);
-  analogWrite(4, 0);
-  analogWrite(5, 0);
-  analogWrite(6, 0);
+void logLn(String text) {
+  // Serial.println(text);
 }
 
 void testRgbCCTCycle() {
   
-  Serial.println("---- ENTERED INTO LED DEMO MODE  ---- ");
+  logLn("---- ENTERED INTO LED DEMO MODE  ---- ");
   
   setColdWhite(0);
   delay(testTimeOffset);
